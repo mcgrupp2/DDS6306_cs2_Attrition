73306c49fb2464ea02e5eec74747360709edf6a4
mcgrupp2 <jrupp@smu.edu>
2019-11-15 18:12:40 +0000

    converted docx to txt to read

--- test.R
+++ test.R
@@ -1,1 +1,16 @@
 #these could be edits
+
+
+#install.packages("tidyverse")
+#install.packages("naniar")
+
+library(plyr)
+library(tidyverse)
+library(naniar)
+
+raw_df <- read.csv("CaseStudy2-data.csv")
+#raw_df 
+
+naniar::gg_miss_var(raw_df)
+
+names(raw_df)
