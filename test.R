e46f219d83735c6bf3bc86ff2e907c0987142f9f
mcgrupp2 <jrupp@smu.edu>
2019-11-18 22:39:31 +0000

    many changes

--- test.R
+++ test.R
@@ -3,11 +3,23 @@
 
 #install.packages("tidyverse")
 #install.packages("naniar")
+#install.packages("caret")
+#install.packages('e1071', dependencies = TRUE)
+#install.packages("mlbench")
+#install.packages("fastDummies")
+#install.packages("xgboost")
+
 
 library(plyr)
 library(tidyverse)
 library(naniar)
 library(GGally)
+library(caret)
+library(e1071)
+library(mlbench)
+library(fastDummies)
+library(xgboost)
+
 
 #Read the raw csv with attrition and salaries
 
@@ -133,6 +145,147 @@ circlize_dendrogram(dend)
 
 
 plot(dend)
+
+
+#~~~~~~~~~~~~~~~~~~~~Feature Selection~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+set.seed(22)
+
+fs_df <- cbind(raw_df[,2], raw_df[,4:36])
+
+fs_df <- dummy_cols(fs_df)
+
+fs_df
+
+dim(fs_df)
+
+
+
+names(fs_df)
+
+f_df <- cbind(fs_df[,1],    
+              fs_df[,3],
+              fs_df[,5:6],   
+              fs_df[,10],    
+              fs_df[,12],
+              fs_df[,13:14], 
+              fs_df[,16],    
+              fs_df[,19:20], 
+              fs_df[,24:25],
+              fs_df[,27:60],
+              fs_df[,62:63]) 
+
+dim(f_df) 
+
+names(f_df)
+
+names(f_df)[[1]] <- "Age"
+names(f_df)[[2]] <- "DailyRate"
+names(f_df)[[5]] <- "EnvironmentSatisfaction"
+names(f_df)[[6]] <-  "HourlyRate"
+names(f_df)[[9]] <- "JobSatisfaction"
+
+#names(f_df)
+
+#fs_df <- dummyVars(" ~ .", data = fs_df)
+
+#fs_df
+
+#f_df <- data.frame(predict(fs_df, newdata = fs_df))
+
+#str(f_df)
+
+#gg_miss_var(f_df)
+
+
+#nums <- sapply(f_df, is.numeric)
+#data.numeric <- f_df[ , nums]
+
+#data.without_na <- na.omit(data.numeric)
+#cor_matrix <- cor(data.without_na)
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~Remove Redundant Features
+
+correlationMatrix <- cor(f_df)#use="pairwise.complete.obs")
+
+gg_miss_var(correlationMatrix)
+
+gg_miss_var(as.data.frame(correlationMatrix))
+
+highlyCorrelated <- findCorrelation(correlationMatrix, cutoff = 0.5)
+
+print(highlyCorrelated)
+
+print(correlationMatrix[highlyCorrelated,])
+
+
+tmp_df <- cbind(f_df, raw_df[,])
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~Rank Features By Importance
+
+
+
+control <- trainControl(method="repeatedcv", number=10, repeats=3)
+# train the model
+model <- train(f_df, raw_df[,3], method="lvq", preProcess="scale", trControl=control)
+# estimate variable importance
+importance <- varImp(model, scale=FALSE)
+# summarize importance
+print(importance)
+# plot importance
+plot(importance)
+
+
+
+#~~~~~~~~~~~~~~~~~~~~Feature Selection (Automatic)------LONG LOAD
+
+set.seed(22)
+
+control <- rfeControl(functions=rfFuncs, method="cv", number=10)
+# run the RFE algorithm
+results <- rfe(f_df, raw_df[,3], sizes=c(1:50), rfeControl=control)
+# summarize the results
+print(results)
+# list the chosen features
+predictors(results)
+# plot the results
+plot(results, type=c("g", "o"))
+
+p_df <- f_df[,results$variables[1:26,4]]
+
+
+
+names(f_df)
+
+p_df
+
+
+m_df <- data.matrix(p_df)
+
+
+t_splt <- round(dim(p_df)[[1]] * .7)
+
+#t_splt
+
+train_data <- m_df[1:t_splt,]
+#train_data
+
+test_data <- m_df[-(1:t_splt),]
+#test_data
+
+#diseaseInfo <- diseaseInfo[sample(1:nrow(diseaseInfo)), ]
+
+#dtrain <- xgb.D
+
+
+
+
+
 
 
 
